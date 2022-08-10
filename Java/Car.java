2bbf268830d2f1395674a423e1ff52cffa2d63db
triplehomicide <lkodo573@learner.dvusd.org>
2022-08-10 11:25:26 -0700

    I like bread

---
+++ Java/Car.java
@@ -0,0 +1,34 @@
+/**
+ * Class Car - nonsense class for debugger demo
+ * 
+ * @author Michael Kölling
+ * @version 13 August 1999
+ */
+public class Car
+{
+    int bruh;
+    private int numberOfFrontSeats;
+    private int numberOfBackSeats;
+
+    /**
+     * Create a car with seat numbers
+     */
+    public Car(int frontSeats, int backSeats)
+    {
+        numberOfFrontSeats = frontSeats;
+        numberOfBackSeats = backSeats;
+    }
+
+    /**
+     * Return the number of seats in the car.
+     */
+    public int seats()
+    {
+        int totalSeats;
+
+        totalSeats = numberOfFrontSeats;
+        totalSeats += numberOfBackSeats;
+
+        return totalSeats;
+    }
+}
